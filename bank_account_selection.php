ea4a6acf11572e4785ea768c914951bf1027f9b7
Cyberwolf101 <lordtimothy055@gmail.com>
2025-09-11 02:28:17 -0700

    try

--- bank_account_selection.php
+++ bank_account_selection.php
@@ -35,6 +35,7 @@ if (!isset($showAmountInput)) {
 </style>
 
 
+
 <!-- ======= Bank Transfer Section ======= -->
 <section id="bank-transfer" class=" m-0 p-0">
     <div class="" style="width:100%;">
