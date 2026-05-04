6241046569a61b1137eaa001ba571056d6fe7ea3
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-05-04 11:05:30 +0100

    good

--- admin/fetchdata.php
+++ admin/fetchdata.php
@@ -1,22 +1,24 @@
 <?php
 
 // Include your database connection here
-include "../connect.php"; 
+include "../connect.php";
 
 
 if (isset($_POST['shop'])) {
   $selectedShop = $_POST['shop'];
   $thedate = $_POST['date'];
 
-  
-  //fetch quantity
-$sql = "SELECT * from chb_inventory where product='$selectedShop'";
-$sql2 = mysqli_query($con,$sql);
-while ($row = mysqli_fetch_array($sql2)) {
-$maxquantity=$row['inventory'];
-} 
 
-echo '<label id="packsLabel">Quantity Deducted</label><input type="number"  max="'.$maxquantity.'" class="form-control" name="quantity" required /><br />';
+  //fetch quantity
+  $sql = "SELECT * from chb_inventory where product='$selectedShop'";
+  $sql2 = mysqli_query($con, $sql);
+  while ($row = mysqli_fetch_array($sql2)) {
+    $maxquantity = $row['inventory'];
+  }
+
+?>
+  <label id="packsLabel">Quantity Deducted</label><input type="number" max="<?php echo $maxquantity ?>" class="form-control" name="quantity" required /><br />
+<?php
 }
 
 ?>
