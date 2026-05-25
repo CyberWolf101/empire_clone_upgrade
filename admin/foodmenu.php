dd139f048ca61d6821a5fc430bb09f899a9924f2
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-05-25 12:51:07 +0100

    finished the credit sales integration on the admin panel ---- AyExpertDev ---- www.ayexpertdev-portfolio.vercel.app

--- admin/foodmenu.php
+++ admin/foodmenu.php
@@ -44,6 +44,7 @@ $items_json = json_encode($items);
               <option value="25">25</option>
               <option value="50">50</option>
               <option value="100">100</option>
+              <option value="200">200</option>
             </select>
           </div>
           <div>
