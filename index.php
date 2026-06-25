6b340e603ea7023fbcb703a721a29eb2bff0ead1
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-06-25 13:08:31 +0100

    index page fixes

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 include "header.php";
 // Helper function to check if a menu link is enabled
 function isMenuLinkEnabled($con, $name)
